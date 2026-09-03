65afd1afc8ead797cd8fee49f2bc6897123c9e64
Orange Robotics <orangerobotics@gmail.com>
2026-09-03 13:22:03 -0400

    Fixed the project being in the project

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,29 +4,84 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.ironmaple.simulation.SimulatedArena;
+import org.ironmaple.simulation.seasonspecific.rebuilt2026.RebuiltFuelOnField;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot  {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
+    initAdvantageKit();
     m_robotContainer = new RobotContainer();
+
+    DriverStation.silenceJoystickConnectionWarning(true);
+
+    if(RobotBase.isSimulation()) {
+      // Obtains the default instance of the simulation world, which is a Crescendo Arena.
+      SimulatedArena.getInstance();
+      // Add a fuel
+      SimulatedArena.getInstance().addGamePiece(new RebuiltFuelOnField(new Translation2d(3, 3)));
+      // Overrides the default simulation
+      // SimulatedArena.overrideInstance(); 
+    }
+  }
+
+  private void initAdvantageKit() {
+    Logger.recordMetadata("projectName", "Noodle-5.0");
+    Logger.addDataReceiver(new NT4Publisher());
+    if (Constants.atCompetition) {
+      Logger.addDataReceiver(new WPILOGWriter()); // <- log to USB stick
+    }
+    // new PowerDistribution();    // Apparently just constructing a PDH
+    //                             // will allow it's values to be logged? 
+    //                             // This is what the advantage kit docs imply at least.
+    // TODO: We may not need this anymore as of 2026. The AdvantageKit docs indicate that this is
+    //       automatically done for us now, provided we use the default CAN ID for our PDH.
+    Logger.start();
+  //   SignalLogger.setPath("/media/sdb1/ctre");
+  //   SignalLogger.start();
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    m_robotContainer.periodic();
+
+    if(RobotBase.isSimulation()) {
+      simulationPeriod();
+    }
   }
+
+public void simulationPeriod() {
+  // Get the positions of the fuel (both on the field and in the air)
+  Pose3d[] fuelPoses = SimulatedArena.getInstance()
+        .getGamePiecesArrayByType("Fuel");
+  // Publish to telemetry using AdvantageKit
+  Logger.recordOutput("FieldSimulation/FuelPositions", fuelPoses);
+  SimulatedArena.getInstance().simulationPeriodic();
+}
 
   @Override
   public void disabledInit() {}
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+    m_robotContainer.drivetrain.allowTeleportsNextPoseUpdate();
+    m_robotContainer.drivetrain.fullyTrustVisionNextPoseUpdate();
+  }
 
   @Override
   public void disabledExit() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,136 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Volts;
+
+import org.ironmaple.simulation.SimulatedArena;
+import org.ironmaple.simulation.drivesims.COTS;
+import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
+import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig;
+import org.littletonrobotics.junction.Logger;
+
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.subsystems.HumanDriver;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.GyroIOMapleSim;
+import frc.robot.subsystems.drivetrain.GyroIOPigeon;
+import frc.robot.subsystems.drivetrain.SwerveModuleIOKraken;
+import frc.robot.subsystems.drivetrain.SwerveModuleIOMapleSim;
 
 public class RobotContainer {
+
+    public final Drivetrain drivetrain;
+    protected final HumanDriver duncan = new HumanDriver(0);
+    final CommandXboxController duncanController;
+
+    private SwerveDriveSimulation swerveDriveSimulation;
+    
   public RobotContainer() {
+    /**** INITIALIZE SUBSYSTEMS ****/
+    if (RobotBase.isReal()) { //TODO put this back to not !
+        // NOODLE OFFSETS: FL -0.184814453125, FR 0.044677734375, BL -0.3349609375, BR 0.088134765625 
+        drivetrain = new Drivetrain( 
+            new GyroIOPigeon(),
+            new SwerveModuleIOKraken(1, 2, -0.260498, 1, "FL", true), 
+            new SwerveModuleIOKraken(3, 4, 0.429199, 2, "FR", false),
+            new SwerveModuleIOKraken(5, 6, -0.033203, 3, "BL", true),
+            new SwerveModuleIOKraken(7, 8,  0.098389, 4, "BR", false) 
+        );
+    } else {
+        // drivetrain = new Drivetrain(
+        //     new GyroIOSim(){},
+        //     new SwerveModuleIOSim(){},
+        //     new SwerveModuleIOSim(){},
+        //     new SwerveModuleIOSim(){},
+        //     new SwerveModuleIOSim(){}
+        // );
+      DriveTrainSimulationConfig driveSimulationConfig = DriveTrainSimulationConfig.Default()
+        // Specify gyro type (for realistic gyro drifting and error simulation)
+        .withGyro(COTS.ofPigeon2())
+        // Specify swerve module (for realistic swerve dynamics)
+        .withSwerveModule(new SwerveModuleSimulationConfig(
+                DCMotor.getKrakenX60(1), // Drive motor is a Kraken X60
+                DCMotor.getKrakenX44(1), // Steer motor is a Falcon 500
+                1.0/Constants.SwerveModuleConstants.driveGearReduction, // Drive motor gear ratio.
+                1.0/Constants.SwerveModuleConstants.steerGearReduction, // Steer motor gear ratio.
+                Volts.of(0.25), // Drive friction voltage.
+                Volts.of(0.2), // Steer friction voltage
+                Inches.of(Constants.SwerveModuleConstants.wheelRadiusMeters), // Wheel radius
+                KilogramSquareMeters.of(0.03), // Steer MOI
+                1.2)) // Wheel COF
+        // Configures the track length and track width (spacing between swerve modules)
+        .withTrackLengthTrackWidth(
+            Meters.of(DrivetrainConstants.wheelbaseMeters), 
+            Meters.of(DrivetrainConstants.trackwidthMeters)
+            )
+        // Configures the bumper size (dimensions of the robot bumper)
+        .withBumperSize(
+            Meters.of(DrivetrainConstants.bumperWidthMeters),
+            Meters.of(DrivetrainConstants.bumperWidthMeters)
+        );
+
+        /* Create a swerve drive simulation */
+        swerveDriveSimulation = new SwerveDriveSimulation(
+                // Specify Configuration
+                driveSimulationConfig,
+                // Specify starting pose
+                new Pose2d(3, 3, new Rotation2d())
+        );
+
+        drivetrain = new Drivetrain(
+            new GyroIOMapleSim(swerveDriveSimulation.getGyroSimulation()),
+            new SwerveModuleIOMapleSim(swerveDriveSimulation.getModules()[0]),
+            new SwerveModuleIOMapleSim(swerveDriveSimulation.getModules()[1]),
+            new SwerveModuleIOMapleSim(swerveDriveSimulation.getModules()[2]),
+            new SwerveModuleIOMapleSim(swerveDriveSimulation.getModules()[3])
+        );
+
+        drivetrain.setPoseMeters(new Pose2d(3, 3, new Rotation2d()));
+
+        SimulatedArena.getInstance().addDriveTrainSimulation(swerveDriveSimulation);
+    }
+
+    duncanController = duncan.getXboxController();
     configureBindings();
+    setDefaultCommands();
   }
 
   private void configureBindings() {}
 
+  public void setDefaultCommands() {
+      drivetrain.setDefaultCommand(driverFullyControlDrivetrain().withName("driveDefualtCommand"));
+      // canLedsCounter.setDefaultCommand(canLedsCounter.solidColorCommand(Color.fromHSV(canLedsCounter.getAllianceHue(), 255, 255)).ignoringDisable(true));
+  }
+
+      private Command driverFullyControlDrivetrain() { return drivetrain.run(() -> {
+        drivetrain.fieldOrientedDrive(duncan.getRequestedFieldOrientedVelocity());
+        Logger.recordOutput("drivetrain/runningDefaultCommand", true);
+        }).finallyDo(() -> {
+            Logger.recordOutput("drivetrain/runningDefaultCommand", false);
+        }).withName("driverFullyControlDrivetrain");
+    }
+
+  /** Called by Robot.java, convenience function for logging. */
+  public void periodic() {
+    Logger.recordOutput("robotContainer/simulatedDrivetrainPoseMeters", swerveDriveSimulation.getSimulatedDriveTrainPose());
+  }
+
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
+
+
 }
